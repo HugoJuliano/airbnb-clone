4c27fac1e1ea0327d5304b1bc0be2c7fddb4310d
HugoJuliano <hugojuliano@hotmail.com.br>
2024-10-08 00:50:21 -0300

    git intermediario

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import './styles.css'
+import NavBar from './components/NavBar'
+import Hero from './components/Hero'
+import Card from './components/Card'
+import data from "./data"
+export default function App() {
+
+  const elementData = data.map((elemento) => {
+    return (
+    <Card
+     img={elemento.coverImg}
+     rating={elemento.stats.rating}
+     reviewCount={elemento.stats.reviewCount}
+     country={elemento.location}
+     title={elemento.title}
+     price={elemento.price}
+     openSpots={elemento.openSpots}
+ />
+);
+});
+    return (
+    <div className="App">
+      
+      <NavBar /> 
+      <Hero />
+      <section className='card-list'>
+          {elementData}
+        </section>
+    </div>
+    );
+  }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+export default function Hero() {
+    return (
+      <section className="hero">
+        <img src="/images/photo-grid.png" className="hero-grid" alt="imagem grid" />
+        <div className="hero-texto">
+          <h1>Online Experiences</h1>
+          <p>
+            Join unique interactive activities led by
+            <br /> one-of-a-kind hosts—all without leaving
+            <br /> home.
+          </p>
+        </div>
+      </section>
+    );
+  }
